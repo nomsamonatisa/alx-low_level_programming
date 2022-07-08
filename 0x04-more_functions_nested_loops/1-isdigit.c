37ab976ec8451d853547507c248b77e14884ccce
Paballo Monatisa <paballomonatisa7@gmail.com>
2022-07-08 09:58:15 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,53 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
 
-/**
- * _isdigit -  checks for checks for a digit (0 through 9).
- * @c: c -  Variable
- * Return: Always 0.
- */
-int _isdigit(int c)
-{
+static inline bool check_input(int n){
+    return 0 <= n && n <= 9;
+}
 
-	if (c >= 48 && c <= 57)
-	{
-		return (1);
-	}
-	return (0);
+void read_row(int x, int y, int a[x][y]){
+    for (int r = 0; r < x; ++r){
+        printf("Enter row %d: ", r);
+        for (int c = 0; c < y; ++c){
+            int value;
+            if(1 != scanf("%d", &value) || check_input(value) == false){
+                printf("Values outside of range.\n");
+                while(getchar() != '\n'); //clear inputs;
+                --r;
+                break;
+            }
+            a[r][c] = value;
+        }
+    }
+}
+
+void compute_row_count(int x, int y, int a[x][y], int counts[]){
+    //memset(counts, 0, sizeof(int)*10);
+    for (int r = 0; r < x; ++r){
+        for (int c = 0; c < y; ++c){
+            ++counts[a[r][c]];
+        }
+    }
+}
+
+void print_total_count(int counts[]){
+    printf("\nTotal count for each digit:\n");
+    for (int i = 0; i < 10; ++i){
+        printf("Digit %d occurs %d time%s\n", i, counts[i], counts[i] > 1 ? "s": "");
+    }
+}
+
+int main (void){
+    int x, y;
+    printf("Enter the size of your array: ");
+    scanf("%d %d", &x, &y);
+
+    int a[x][y];
+    int counts[10] = {0};
+
+    read_row(x, y, a);
+    compute_row_count(x, y, a, counts);
+    print_total_count(counts);
+
+    return 0;
 }
