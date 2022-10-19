6d9a84fbf1ef45afb72dc9c68c3484383cdcc882
Monatisa Mamosa <paballomonatisa7@gmail.com>
2022-10-19 11:13:03 -0700

    This program prints the largest of three integers

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-This is a readme for 0x02-functions_nested_loops
+0-putchar
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c)
+
+void print _alphabet(void);
+void print _alphabet_*10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_tables(void);
+int add(int, int);
+void print_to_98(int n)
+int _putchar(char c);
+
+#endif
