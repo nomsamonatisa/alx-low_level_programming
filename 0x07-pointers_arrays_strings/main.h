73906f2231fc827eb84b93ad00345d8fd5418dd1
Monatisa Mamosa <paballomonatisa7@gmail.com>
2022-10-31 19:36:50 -0700

    added main.h and main.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,10 @@
-_putchar(char c);
-*_memset(char *s, char b, unsigned int n);
-*_memcpy(char *dest, char *src, unsigned int n);
-*_strchr(char *s, char c);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-*_strpbrk(char *s, char *accept);
-*_strstr(char *haystack, char *needle);
-print_chessboard(char (*a)[8]);
-set_string(char **s, char *to);
-print_diagsums(int *a, int size);
-clear
-:wq
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
