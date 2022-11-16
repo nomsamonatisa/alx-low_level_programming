9c83486ab281d0110bb6e3b861a861feecc9af98
Monatisa Mamosa <paballomonatisa7@gmail.com>
2022-11-16 09:38:52 -0800

    added a new code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+#include "function_pointers.h"
+
 /**
  * print_name - prints the name
- * @name: name
- * @f: function
+ * @name: name to print
+ * @f: pointer to the print fuction
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
 	f(name);
 }
